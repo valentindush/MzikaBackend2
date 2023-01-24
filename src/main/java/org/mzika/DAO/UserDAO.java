11a371d8f24eeec5a994b9a5815c0dd71865902e
Valentin <93974733+valentindush@users.noreply.github.com>
2023-01-24 15:21:27 +0200

    Added UserDAOimpl

--- src/main/java/org/mzika/DAO/UserDAO.java
+++ src/main/java/org/mzika/DAO/UserDAO.java
@@ -4,7 +4,7 @@ import org.mzika.orm.User;
 
 public interface UserDAO {
     public User saveUser(User user);
-    public User getUser(User user);
+    public User getUser(int id);
     public User updateUser(User user);
     public  boolean deleteUser(int id);
 }
---
+++ src/main/java/org/mzika/DAOimpl/UserDAOimpl.java
@@ -0,0 +1,64 @@
+package org.mzika.DAOimpl;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.mzika.DAO.UserDAO;
+import org.mzika.orm.User;
+
+public class UserDAOimpl implements UserDAO {
+
+    private Session session;
+    Transaction transaction = null;
+
+    public UserDAOimpl(Session session){
+        this.session = session;
+    }
+
+    @Override
+    public User saveUser(User user) {
+
+        try {
+
+            transaction = this.session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+
+            return user;
+
+        }catch (Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    @Override
+    public User getUser(int id) {
+
+        try{
+
+            transaction = this.session.beginTransaction();
+            return (User) session.get(User.class, id);
+
+        }catch (Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public User updateUser(User user) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteUser(int id) {
+        return false;
+    }
+}
